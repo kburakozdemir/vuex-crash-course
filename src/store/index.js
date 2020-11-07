3fe4b60609e195961a8ef417933234743cdac1dc
K. Burak Ozdemir <kburakozdemir@gmail.com>
2020-11-07 15:15:58 +0300

    Add store

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+// Entry module for Vuex
+import Vuex from "vuex";
+import Vue from "vue";
+import todos from "./modules/todos";
+
+// Load Vuex
+Vue.use(Vuex);
+
+//Create store
+export default new Vuex.Store({
+    modules: {
+        todos
+    }
+});
