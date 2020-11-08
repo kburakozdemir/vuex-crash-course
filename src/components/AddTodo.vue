c18c2e850ae1d2edda9ce9a208f11d03d29efbc4
K. Burak Ozdemir <kburakozdemir@gmail.com>
2020-11-08 20:19:32 +0300

    Add form

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h3>Add Todo</h3>
+    <div class="add">
+      <form>
+        <input type="text" v-model="title" placeholder="Add Todo..." />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
   </div>
 </template>
 
